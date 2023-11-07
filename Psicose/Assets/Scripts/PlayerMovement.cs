c9449a61254276e29304ac10836c05e931660a6a
Yan Hideki Yashiki <yan.yashiki@gmail.com>
2023-11-07 09:58:08 -0300

    Dash Salvov1

--- Psicose/Assets/Scripts/PlayerMovement.cs
+++ Psicose/Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f; // Velocidade de movimento do jogador.
+  
+    public float speed = 5f;
+    [SerializeField] public float dashSpeed = 5f;
+    [SerializeField] public float dashDuration = 2f;
+    [SerializeField] public float dashCooldown = 3f;
 
-    private Rigidbody2D rb;
+    bool isDashing;
+
+    public Rigidbody2D rb;
 
     Vector2 movement;
     public Animator animator;
@@ -19,19 +26,23 @@ public class PlayerMovement : MonoBehaviour
 
     void Start()
     {
+        
         rb = GetComponent<Rigidbody2D>();
         this.moveDirection = MoveDirection.Right;
     }
 
     void Update()
     {
+        if(isDashing)
+        {
+            return;
+        }
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Velocidade", movement.sqrMagnitude);
-
 
         if (movement.x > 0)
         {
@@ -46,7 +57,22 @@ public class PlayerMovement : MonoBehaviour
             this.moveDirection = MoveDirection.Left;
         }
 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(Dash());
+        }
+
+      
     }
+
+    private IEnumerator Dash()
+    {
+        isDashing = true;
+        rb.velocity = new Vector2(movement.x * dashSpeed, movement.y * dashSpeed);
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+    }
+    
 
     void FixedUpdate()
     {
