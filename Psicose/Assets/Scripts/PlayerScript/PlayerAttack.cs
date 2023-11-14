58a919edae91a0fbee6ebb383637b577cdc9da01
Yan Hideki Yashiki <yan.yashiki@gmail.com>
2023-11-14 09:48:30 -0300

    Player Resolvido
    Diga fi cansei

--- Psicose/Assets/Scripts/PlayerScript/PlayerAttack.cs
+++ Psicose/Assets/Scripts/PlayerScript/PlayerAttack.cs
@@ -38,83 +38,82 @@ public class PlayerAttack : MonoBehaviour
     public void Update()
     {
 
-       if (Input.GetKeyDown(KeyCode.Space))
-       {
-                Attack();
-       }
-
-
-
-        void OnDrawGizmos()
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Gizmos.color = Color.white;
-            if (this.attackPointR != null)
-            {
+            Attack();
+        }
+    }
+
+
+
+     private void OnDrawGizmos()
+     {
+         Gizmos.color = Color.white;
+         if (this.attackPointR != null)
+         {
                 Gizmos.DrawWireSphere(this.attackPointR.position, this.attackRange);
-            }
+         }
 
-            if (this.attackPointL != null)
-            {
-                Gizmos.DrawWireSphere(this.attackPointL.position, this.attackRange);
-            }
+         if (this.attackPointL != null)
+         {
+             Gizmos.DrawWireSphere(this.attackPointL.position, this.attackRange);
+         }
 
 
-            Transform attackPoint;
-            if (this.player.moveDirection == MoveDirection.Right)
-            {
-                attackPoint = this.attackPointR;
-            }
-            else
-            {
-                attackPoint = this.attackPointL;
-            }
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(attackPoint.position, this.attackRange);
+         Transform attackPoint;
+         if (this.player.moveDirection == MoveDirection.Right)
+         {
+           attackPoint = this.attackPointR;
+         }
+         else
+         {
+           attackPoint = this.attackPointL;
+         }
+         Gizmos.color = Color.red;
+         Gizmos.DrawWireSphere(attackPoint.position, this.attackRange);
+     }
+
+
+     private void Attack()
+     {
+        Transform attackPoint;
+        if (this.player.moveDirection == MoveDirection.Right)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            attackPoint = this.attackPointR;
+
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            attackPoint = this.attackPointL;
         }
 
 
-        void Attack()
+        if (Input.GetKeyDown(KeyCode.Space) && !isAttacking)
         {
-            if (Input.GetKeyDown(KeyCode.Space) && !isAttacking)
-            {
                 isAttacking = true;
-            }
-
-
-
         }
 
+
+
+
+
+
+        Collider2D[] collidersEnemy = Physics2D.OverlapCircleAll(attackPoint.position, this.attackRange, this.EnemyLayers);
+        if (collidersEnemy != null)
         {
-
-            Transform attackPoint;
-            if (this.player.moveDirection == MoveDirection.Right)
-            {
-                transform.localScale = new Vector3(1, 1, 1);
-                attackPoint = this.attackPointR;
-
-            }
-            else
-            {
-                transform.localScale = new Vector3(-1, 1, 1);
-                attackPoint = this.attackPointL;
-            }
-
-
-
-            Collider2D[] collidersEnemy = Physics2D.OverlapCircleAll(attackPoint.position, this.attackRange, this.EnemyLayers);
-            if (collidersEnemy != null)
-            {
                 //verifica a existencia de inimigos na area do golpe.
 
-                foreach (Collider2D colliderEnemy in collidersEnemy)
-                {
-                    Enemy_Follow enemy = colliderEnemy.GetComponent<Enemy_Follow>();
-                    if (enemy != null)
-                    {
-                        enemy.DamageHit(attackDamage);
-                    }
-                }
-            }
+           foreach (Collider2D colliderEnemy in collidersEnemy)
+           {
+               Enemy_Follow enemy = colliderEnemy.GetComponent<Enemy_Follow>();
+                 if (enemy != null)
+                 {
+                     enemy.DamageHit(attackDamage);
+                 }
+           }
+        }
 
             /*Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, EnemyLayers);
 
@@ -123,9 +122,6 @@ public class PlayerAttack : MonoBehaviour
                 enemy.GetComponent<Enemy_Follow>().DamageHit(attackDamage);
             }
             */
-        }
+     }
 
-
-
-    }
 }
